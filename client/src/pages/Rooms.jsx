c1929a7c5ecfb2ee3a5904c683f43d45bd2f9c15
Carl Ivan Ken Simbulan <carlivan252003@gmail.com>
2026-07-13 09:57:12 +0800

    S

--- client/src/pages/Rooms.jsx
+++ client/src/pages/Rooms.jsx
@@ -59,7 +59,6 @@ export default function Rooms() {
             ))}
           </div>
         </div>
-      </div>
 
       {/* Content */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
